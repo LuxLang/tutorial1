12574e487593668615568c44730380a12af0328e
Eduardo Julian <eduardoejp@gmail.com>
2015-12-24 22:41:25 -0400

    - Updated standard-library version to 0.3.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lux/tutorial1 "0.1.0-SNAPSHOT"
   :plugins [[lux/lein "0.1.0"]]
-  :dependencies [;; [lux/stdlib "0.3.0"]
+  :dependencies [[lux/stdlib "0.3.1"]
                  [io.vertx/vertx-web "3.0.0"]]
   :lux/program "tutorial1")

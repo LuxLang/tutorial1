4d87b29e913446d7f9973a22f149caf1eaffc5ad
Eduardo Julian <eduardoejp@gmail.com>
2015-12-27 09:42:20 -0400

    - Updated code to avoid name clashes in case-insensitive file-systems.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lux/tutorial1 "0.1.0-SNAPSHOT"
   :plugins [[lux/lein "0.1.0"]]
-  :dependencies [[lux/stdlib "0.3.1"]
+  :dependencies [[lux/stdlib "0.3.2-SNAPSHOT"]
                  [io.vertx/vertx-web "3.0.0"]]
   :lux/program "tutorial1")

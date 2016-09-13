82eb50e1aa99b8961257cb90b7d6e2153470980d
Eduardo Julian <eduardoejp@gmail.com>
2016-09-13 18:02:24 -0400

    - Ported to Lux 0.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject lux/tutorial1 "0.1.0-SNAPSHOT"
-  :plugins [[lux/lein "0.1.0"]]
-  :dependencies [[lux/stdlib "0.3.2-SNAPSHOT"]
+  :plugins [[com.github.luxlang/lein-luxc "0.3.0"]]
+  :dependencies [[com.github.luxlang/lux-stdlib "0.4.0"]
                  [io.vertx/vertx-web "3.0.0"]]
-  :lux/program "tutorial1")
+  :source-paths ["source"]
+  :lux/program "tutorial1"
+  )
